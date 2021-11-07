1ec94868d9f153cb2d6a24dabdb7a225d8f03081
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-11-07 22:30:14 +0100

    Creación de fichero a partir de los datos exitosa, falta implementar la lectura de datos.

--- src/t2/evaluable2/AEv2.java
+++ src/t2/evaluable2/AEv2.java
@@ -1,15 +1,61 @@
 package t2.evaluable2;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AEv2 {
 
+	public String[] leerNEO(String linea) {
+		String[] propiedadesNEO = linea.split(",");
+		System.out.println("El NEO " + propiedadesNEO[0] + ", tiene la posicion " + propiedadesNEO[1]
+				+ " y tiene una velocidad de " + propiedadesNEO[2] + " km/s.");
+		return propiedadesNEO;
+	}
+
+	public void lanzarProbabilidad(String[] propiedadesNEO) {
+		String clase = "t2.evaluable2.CalculaProbabilidad";
+		try {
+			File directorioProbabilidad = new File("...");
+			File fichProbabilidad = new File(propiedadesNEO[0]);
+			String javaHome = System.getProperty("java.home");
+			String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+			String classpath = System.getProperty("java.class.path");
+			System.out.println(classpath);
+			String className = clase;
+
+			List<String> command = new ArrayList<>();
+			command.add(javaBin);
+			command.add("-cp");
+			command.add(classpath);
+			command.add(className);
+			command.add(propiedadesNEO[0].toString());
+			command.add(propiedadesNEO[1].toString());
+			command.add(propiedadesNEO[2].toString());
+
+			System.out.println(command);
+
+			ProcessBuilder builder = new ProcessBuilder(command);
+			builder.directory(directorioProbabilidad);
+			builder.redirectOutput(fichProbabilidad);
+			Process process = builder.start();
+			process.waitFor();
+			System.out.println(process.exitValue());
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void getResultadoFichero(String nombreFichero) {
-		
+
 		try {
 			FileInputStream fichero = new FileInputStream(nombreFichero);
 			InputStreamReader isr = new InputStreamReader(fichero);
@@ -19,25 +65,44 @@ public class AEv2 {
 			System.out.println("La probabilidad de que el NEO colisione es del " + probabilidad + "%");
 			if (probabilidad > 10) {
 				System.err.println("Alerta Mundial!! Riesgo elevado de impacto terrestre");
-			}else {
+			} else {
 				System.out.println("Riesgo bajo de impacto terrestre");
 			}
-		}catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		int numeroCores = Runtime.getRuntime().availableProcessors();
 		System.out.println(numeroCores);
-		for (int i = 0; i < 4; i++) {
-
+		AEv2 objeto = new AEv2();
+		try {
+			File f = new File("NEOs.txt");
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			String linea = br.readLine();
+			
+			for (int i = 0; i < numeroCores; i++) {
+				String[] propiedadesNEO = objeto.leerNEO(linea);
+				objeto.lanzarProbabilidad(propiedadesNEO);
+				linea = br.readLine();
+			}
+			br.close();
+			fr.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 
 	}
 
--- src/t2/evaluable2/CalculaProbabilidad.java
+++ src/t2/evaluable2/CalculaProbabilidad.java
@@ -13,14 +13,15 @@ public class CalculaProbabilidad {
 		Math.pow( ((posicionNEO-posicionTierra)/(posicionNEO+posicionTierra)), 2);
 		return resultado;
 	}
-
-	public static double main(String[] args) {
+	
+	public static void main(String[] args) {
 		CalculaProbabilidad cp = new CalculaProbabilidad();
 		double n1 = Double.parseDouble(args[1]);
 		double n2 = Double.parseDouble(args[2]);
 		double resultado = cp.calcula(n1, n2);
 		System.out.println(resultado);
-		return resultado;
+		
+		
 	}
 
 }
