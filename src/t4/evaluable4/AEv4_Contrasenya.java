06d21908921fc283365490a524bcaca5e5a0566a
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-02-09 13:19:26 +0100

    clase contrasenya

--- src/t4/evaluable4/AEv4_Contrasenya.java
+++ src/t4/evaluable4/AEv4_Contrasenya.java
@@ -1,5 +1,43 @@
 package t4.evaluable4;
 
-public class AEv4_Contrasenya {
+import java.io.Serializable;
 
+public class AEv4_Contrasenya implements Serializable{ // He creado la siguiente clase con el nombre de   
+								// los atributos, luego clic derecho, Source y los distintos campos,
+								// Constructor bacío, constructor con atributos, getters y setters y toString().
+	
+	String contrasenyaPlana, contrasenyaEncriptada;
+
+	public AEv4_Contrasenya() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AEv4_Contrasenya(String contrasenyaPlana, String contrasenyaEncriptada) {
+		super();
+		this.contrasenyaPlana = contrasenyaPlana;
+		this.contrasenyaEncriptada = contrasenyaEncriptada;
+	}
+
+	public String getContrasenyaPlana() {
+		return contrasenyaPlana;
+	}
+
+	public void setContrasenyaPlana(String contrasenyaPlana) {
+		this.contrasenyaPlana = contrasenyaPlana;
+	}
+
+	public String getContrasenyaEncriptada() {
+		return contrasenyaEncriptada;
+	}
+
+	public void setContrasenyaEncriptada(String contrasenyaEncriptada) {
+		this.contrasenyaEncriptada = contrasenyaEncriptada;
+	}
+
+	@Override
+	public String toString() {
+		return "AEv4_Contrasenya [contrasenyaPlana=" + contrasenyaPlana + ", contrasenyaEncriptada="
+				+ contrasenyaEncriptada + "]";
+	}
 }
